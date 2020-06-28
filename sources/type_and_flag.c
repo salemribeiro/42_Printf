865557ab6450a9d6703821e986cd115fafb9fd77
SalemRibeiro <salem.ribeiro@sapo.pt>
2020-06-28 00:15:47 -0300

    Melhoria no código com a inclusão de macros

--- sources/type_and_flag.c
+++ sources/type_and_flag.c
@@ -6,102 +6,98 @@
 /*   By: sfreitas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 22:57:57 by sfreitas          #+#    #+#             */
-/*   Updated: 2020/04/29 22:58:33 by sfreitas         ###   ########.fr       */
+/*   Updated: 2020/06/28 00:05:57 by salem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+#define FORBIDEN1 "uoxXcspn\0"
+#define FORBIDEN2 "diucspn\0"
+#define FORBIDEN3 "cdpn\0"
+
 void	solve_plus_flag_with_types(int *flags, char specifier)
 {
-	char	forbiden[9];
-	int		temp;
-	int		valid;
 	int		i;
+	int		tmp;
+	int		valid;
 
-	ft_strlcpy(forbiden, "uoxXcspn", 9);
-	temp = (*flags | 30) ^ 30;
-	valid = 0;
 	i = 0;
-	while (forbiden[i])
+	valid = 0;
+	tmp = (*flags | 30) ^ 30;
+	while (FORBIDEN1[i])
 	{
-		if (forbiden[i] == specifier)
+		if (FORBIDEN1[i] == specifier)
 			valid = 1;
 		i++;
 	}
-	if (temp == 1 && valid == 1)
+	if (tmp == 1 && valid == 1)
 		*flags = *flags ^ 1;
 }
 
 void	solve_minusflag_with_types(int *flags, char specifier)
 {
-	int temp;
+	int tmp;
 	int i;
 
-	temp = (*flags | 29)  ^ 29;
-	if (temp == 2 && specifier == 'n')
+	tmp = (*flags | 29)  ^ 29;
+	if (tmp == 2 && specifier == 'n')
 		*flags = *flags ^ 2;
 }
 
 void	solve_hash_flag_with_types(int *flags, char specifier)
 {
-	char	forbiden[8];
-	int		temp;
-	char	valid;
 	int		i;
+	int		tmp;
+	char	valid;
 
-	ft_strlcpy(forbiden, "diucspn", 8);
-	temp = (*flags | 27) ^ 27;
-	valid = 0;
 	i = 0;
-	while (forbiden[i])
+	valid = 0;
+	tmp = (*flags | 27) ^ 27;
+	while (FORBIDEN2[i])
 	{
-		if (forbiden[i] == specifier)
+		if (FORBIDEN2[i] == specifier)
 			valid = 1;
 		i++;
 	}
-	if (temp == 4 && valid == 1)
+	if (tmp == 4 && valid == 1)
 		*flags = *flags ^ 4;
 }
 
 void	solve_zero_flag_with_types(int *flags, char specifier)
 {
-	char	forbiden[5];
-	int		temp;
-	char	valid;
 	int		i;
+	int		tmp;
+	char	valid;
 
-	ft_strlcpy(forbiden, "cspn", 5);
-	temp = (*flags | 23) ^ 23;
+	tmp = (*flags | 23) ^ 23;
 	valid = 0;
 	i = 0;
-	while (forbiden[i])
+	while (FORBIDEN3[i])
 	{
-		if (forbiden[i] == specifier)
+		if (FORBIDEN3[i] == specifier)
 			valid = 1;
 		i++;
 	}
-	if (temp == 8 && valid == 1)
+	if (tmp == 8 && valid == 1)
 		*flags = *flags ^ 8;
 }
 
 void	solve_spaceflag_with_types(int *flags, char specifier)
 {
-	char	forbiden[9];
-	int		temp;
-	char	valid;
 	int		i;
+	int		tmp;
+	char	valid;
 
-	ft_strlcpy(forbiden, "uoxXcspn", 9);
-	temp = (*flags | 15) ^ 15;
+	tmp = (*flags | 15) ^ 15;
 	valid = 0;
 	i = 0;
-	while (forbiden[i])
+	while (FORBIDEN1[i])
 	{
-		if (forbiden[i] == specifier)
+		if (FORBIDEN1[i] == specifier)
 			valid = 1;
 		i++;
 	}
-	if (temp == 16 && valid == 1)
+	if (tmp == 16 && valid == 1)
 		*flags = *flags ^ 16;
 }

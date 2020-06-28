508466ac651f63008aa9b5e16f05b798d8e6ed63
SalemRibeiro <salem.ribeiro@sapo.pt>
2020-06-28 00:16:24 -0300

    Melhoria no código com a inclusão de macros

--- sources/specifier_options.c
+++ sources/specifier_options.c
@@ -6,34 +6,33 @@
 /*   By: sfreitas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 22:57:57 by sfreitas          #+#    #+#             */
-/*   Updated: 2020/04/29 22:58:33 by sfreitas         ###   ########.fr       */
+/*   Updated: 2020/06/27 23:55:08 by salem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+#define SPECIFIER "diuoxXfFeEgGaAcspn%\0"
 int		specifier_options(char *subtext)
 {
 	size_t	i;
 	size_t	j;
 	char	*jump;
-	char	specifier[20];
 
 	i = 1;
 	j = 0;
 	jump = 0;
-	ft_strlcpy(specifier,"diuoxXfFeEgGaAcspn%", 20);
 	while (subtext[i])
 	{
-		while (specifier[j] && subtext[i] != specifier[j])
+		while (SPECIFIER[j] && subtext[i] != SPECIFIER[j])
 			j++;
-		if (specifier[j])
+		if (SPECIFIER[j])
 		{
-			jump = flag_options(ft_substr(subtext, 1, i), specifier[j]);
+			jump = flag_options(ft_substr(subtext, 1, i), SPECIFIER[j]);
 			jump = width_options(jump);
-			jump = precision_options(jump, specifier[j]);
+			jump = precision_options(jump, SPECIFIER[j]);
 			jump = length_options(jump);
-			big_hub (specifier[j], 5);
+			big_hub (SPECIFIER[j], 5);
 			return(i + 1);
 		}
 		j = 0;

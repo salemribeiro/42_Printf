2c817f50fa55903bcecf552752a58f4293598183
SalemRibeiro <salem.ribeiro@sapo.pt>
2020-07-03 10:23:21 -0300

    Reformulação na nomenclatura de variáveis

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: sfreitas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/08 23:03:41 by sfreitas          #+#    #+#             */
-/*   Updated: 2020/06/27 21:59:27 by salem            ###   ########.fr       */
+/*   Updated: 2020/06/29 23:22:49 by salem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,20 @@
 
 int			ft_printf(const char *format, ...)
 {
-	va_list			item;
-	struct hub_op	options;
-	char			*text;
+	va_list		item;
+	t_options	opt;
+	char		*text;
 
 	va_start(item, format);
 	while(search_percent((char*)format))
 	{
-		options = big_hub(-1, -1);
-		if (options.width == -1)
-			options = big_hub(va_arg(item, int), 2);
-		if (options.precision == -1)
-			options = big_hub(va_arg(item, int), 3);
-		message_arg_solve(va_arg(item, void*), options.specifier);
-		options = big_hub(0, 0);
+		opt = big_hub(-1, -1);
+		if (opt.width == -1)
+			opt = big_hub(va_arg(item, int), 2);
+		if (opt.precision == -1)
+			opt = big_hub(va_arg(item, int), 3);
+		message_arg_solve(va_arg(item, void*), opt.specifier);
+		opt = big_hub(0, 0);
 	}
 	ft_printstr(message_buffer('\0'));
 	va_end(item);

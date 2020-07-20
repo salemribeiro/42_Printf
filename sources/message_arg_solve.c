669512d380fd806df24da31b50d0a97a5f9e9ae0
SalemRibeiro <salem.ribeiro@sapo.pt>
2020-07-19 21:41:35 -0300

    Retirada das função que tratram diretamente os tipos

--- sources/message_arg_solve.c
+++ sources/message_arg_solve.c
@@ -6,7 +6,7 @@
 /*   By: sfreitas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/08 23:09:08 by sfreitas          #+#    #+#             */
-/*   Updated: 2020/07/14 23:28:25 by salem            ###   ########.fr       */
+/*   Updated: 2020/07/19 14:30:01 by salem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,47 +17,11 @@
 ** para sua função de tratamento correspondente
 */
 
-void	message_arg_solve(void *value, char specifier)
+void	message_arg_solve(va_list item, char specifier)
 {
 	if (specifier == 'd' || specifier == 'i')
-		store_int(value);
-	if (specifier == 'f')
-		store_float((float*)value);
+		store_int(va_arg(item, long int));
 }
-
-/*
-** Tratamento de argumento do tipo inteiro
-*/
-
-void	store_int (void *value)
-{
-	t_options	opt;
-	char		*text;
-	char		c;
-
-	opt = big_hub(-1, -1);
-	text = ft_itoa((int)value);
-	if (opt.precision > 0 && ft_strlen(text))
-		text = put_zero(text, opt.precision);
-	if ((opt.flags & PLUS) == PLUS)
-		text = ft_strjoin("+", text);
-	if ((opt.flags & SPACE) == SPACE)
-		text = ft_strjoin(" ", text);
-	send_buffer(text);
-}
-
-void	store_float (float *value)
-{
-	float teste = *value;
-	int	integer;
-	long long int	fraction;
-	long double		temp;
-
-	integer = (int)teste;
-	printf("Inteiro retirado: %d\n", integer);
-}
-
-
 
 char	*put_zero(char *source, int total)
 {

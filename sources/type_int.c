d58dd54fd799a72cb7c43d17d5c6e2ffdd13db86
SalemRibeiro <salem.ribeiro@sapo.pt>
2020-07-29 09:34:54 -0300

    Troca de envido de dados para variável global

--- sources/type_int.c
+++ sources/type_int.c
@@ -6,7 +6,7 @@
 /*   By: salem <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 13:19:01 by salem             #+#    #+#             */
-/*   Updated: 2020/07/23 22:42:21 by salem            ###   ########.fr       */
+/*   Updated: 2020/07/28 21:00:24 by salem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,25 @@
 
 void	store_int (long long int value)
 {
-	t_options	opt;
 	char		*text;
 	char		c;
 
-	opt = big_hub(-1, -1);
-	if (opt.length == LONG)
+	if (parameters.length == LONG)
 		value = (long long int)(long int) value;
-	else if (opt.length == HH)
+	else if (parameters.length == HH)
 		value = (long long int)(char) value;
-	else if (opt.length == H)
+	else if (parameters.length == H)
 		value = (long long int)(short int) value;
 	if (value < 0)
 		send_buffer("-");
-	text = ft_itoa_base(value, opt.specifier);
+	text = ft_itoa_base(value, parameters.specifier);
 	
-	if (opt.precision > 0 && ft_strlen(text))
-		text = put_zero(text, opt.precision);
-	if ((opt.flags & PLUS) == PLUS && value >= 0)
+	if (parameters.precision > 0 && ft_strlen(text))
+		text = put_zero(text, parameters.precision);
+	if ((parameters.flags & PLUS) == PLUS && value >= 0)
 		text = ft_strjoin("+", text);
-	if ((opt.flags & SPACE) == SPACE)
+	if ((parameters.flags & SPACE) == SPACE)
 		text = ft_strjoin(" ", text);
-
 	send_buffer(text);
 }
 

c188b2f0a8ce8ebf25662fcef4af6ba722422f2c
SalemRibeiro <salem.ribeiro@sapo.pt>
2020-08-02 22:49:00 -0300

    alteração da maneira que os parametros são passados, inicio do tratamento de sinais para os hex oct inteiros

--- sources/type_int.c
+++ sources/type_int.c
@@ -6,27 +6,20 @@
 /*   By: salem <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 13:19:01 by salem             #+#    #+#             */
-/*   Updated: 2020/07/31 00:10:56 by salem            ###   ########.fr       */
+/*   Updated: 2020/08/02 22:45:21 by salem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	store_int (long long int value)
+void			store_int (long long int value)
 {
-	char		*text;
-	char		c;
+	unsigned long long int	tmp;
+	char					*text;
+	char					c;
 
-	if (parameters.length == LONG)
-		value = (long long int)(long int) value;
-	else if (parameters.length == HH)
-		value = (long long int)(char) value;
-	else if (parameters.length == H)
-		value = (long long int)(short int) value;
-	if (value < 0 && parameters.specifier != 'x' && parameters.specifier != 'X')
-		send_buffer("-");
-	text = ft_itoa_base(value, parameters.specifier);
-	
+	text = ft_itoa_base(convert_int(value), parameters.specifier);
+
 	if (parameters.precision > 0 && ft_strlen(text))
 		text = put_zero(text, parameters.precision);
 	if ((parameters.flags & PLUS) == PLUS && value >= 0)
@@ -36,3 +29,28 @@ void	store_int (long long int value)
 	send_buffer(text);
 }
 
+long long int	convert_int(long long int value)
+{
+	unsigned long long int	tmp;
+
+	tmp = 0;
+	if (parameters.length == LONG)
+		value = (long long int)(long int)value;
+	else if (parameters.length == 0)
+		value = (long long int)(int)value;
+	else if (parameters.length == H)
+		value = (long long int)(short int)value;
+	else if (parameters.length == HH)
+		value = (long long int)(char)value;
+	if (value < 0 && parameters.specifier != 'x' && parameters.specifier != 'X')
+	{
+		send_buffer("-");
+		value = value * (-1);
+	}
+	else if (parameters.specifier == 'x' || parameters.specifier == 'X')
+	{
+		tmp = value;
+		tmp = tmp & value;
+	}
+	return (value);
+}

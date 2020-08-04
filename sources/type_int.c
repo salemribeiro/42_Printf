87a62c7083478367d7791333ff2280599692d120
SalemRibeiro <salem.ribeiro@sapo.pt>
2020-08-04 09:52:36 -0300

    inclusão de tratamento de inteiros sem sinal

--- sources/type_int.c
+++ sources/type_int.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/* ************************************************************************* */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   type_int.c                                         :+:      :+:    :+:   */
@@ -6,17 +6,16 @@
 /*   By: salem <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 13:19:01 by salem             #+#    #+#             */
-/*   Updated: 2020/08/02 22:45:21 by salem            ###   ########.fr       */
+/*   Updated: 2020/08/04 09:33:11 by salem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void			store_int (long long int value)
+void	store_int(long long int value)
 {
-	unsigned long long int	tmp;
-	char					*text;
-	char					c;
+	char	*text;
+	char	c;
 
 	text = ft_itoa_base(convert_int(value), parameters.specifier);
 
@@ -29,28 +28,50 @@ void			store_int (long long int value)
 	send_buffer(text);
 }
 
+void	store_u_int(unsigned long long int value)
+{
+	char	*text;
+	char	c;
+
+	text = ft_u_itoa_base(convert_u_int(value), parameters.specifier);
+	if (parameters.precision > 0 && ft_strlen(text))
+		text = put_zero(text, parameters.precision);
+	if ((parameters.flags & PLUS) == PLUS && value >= 0)
+		text = ft_strjoin("+", text);
+	if ((parameters.flags & SPACE) == SPACE)
+		text = ft_strjoin(" ", text);
+	send_buffer(text);
+}
+
 long long int	convert_int(long long int value)
 {
-	unsigned long long int	tmp;
-
-	tmp = 0;
 	if (parameters.length == LONG)
-		value = (long long int)(long int)value;
+		value  = (long long int)(long int)value;
 	else if (parameters.length == 0)
 		value = (long long int)(int)value;
 	else if (parameters.length == H)
 		value = (long long int)(short int)value;
 	else if (parameters.length == HH)
 		value = (long long int)(char)value;
-	if (value < 0 && parameters.specifier != 'x' && parameters.specifier != 'X')
+	if (value < 0)
 	{
 		send_buffer("-");
 		value = value * (-1);
+		if ((parameters.flags & PLUS) == PLUS)
+			parameters.flags = parameters.flags ^ PLUS;
 	}
-	else if (parameters.specifier == 'x' || parameters.specifier == 'X')
-	{
-		tmp = value;
-		tmp = tmp & value;
-	}
+	return (value);
+}
+
+unsigned long long int	convert_u_int(unsigned long long int value)
+{
+	if (parameters.length == LONG)
+		value  = (unsigned long long int)(unsigned long int)value;
+	else if (parameters.length == 0)
+		value = (unsigned long long int)(unsigned int)value;
+	else if (parameters.length == H)
+		value = (unsigned long long int)(unsigned short int)value;
+	else if (parameters.length == HH)
+		value = (unsigned long long int)(unsigned char)value;
 	return (value);
 }

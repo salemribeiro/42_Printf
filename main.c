e6b7388cb54ea0d1f2427ab1ee9ef2cb5f8c3b8d
Salem Ribeiro <salem.ribeiro@sapo.pt>
2020-08-06 20:28:09 -0300

    inclusão de novos testes

--- main.c
+++ main.c
@@ -10,20 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "salem_printflover.h"
+#include "ft_printf.h"
 
 int main ()
 {
 	int	nint = -123454;
 	long double pnint = 25;
 
-	type_table();
-	test_int_only_signal();
-	test_int_signal_with_space();
-	test_int_signal_with_hash();
-/*
-	ft_printf("% d salem\n",1234);
-	printf("% d salem\n",1234);
-*/
+//	type_table();
+//	test_int_only_signal();
+//	test_int_signal_with_space();
+//	test_int_signal_with_hash();
+	ft_printf("Caractere:\n");
+	ft_printf("1 - %c, 2 - %c 12345%c6789\n", 'A', 'B','\0');
+	ft_printf("String\n");
+	ft_printf("texto = %s\n", "salem.ribeiro@gepro.eng.br");
 	return (0);
 }

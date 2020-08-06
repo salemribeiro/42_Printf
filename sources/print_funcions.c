a2134d69ca775ca8963aa66e898e8747e614720b
Salem Ribeiro <salem.ribeiro@sapo.pt>
2020-08-06 20:31:26 -0300

    melhoria na lógica para tratamento do tipo char

--- sources/print_funcions.c
+++ sources/print_funcions.c
@@ -11,15 +11,30 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
 int		ft_printstr(char *text)
 {
 	int i;
+	int j;
+	int k;
+	char c;
 
 	i = 0;
+	j = 0;
+	k = 0;
+	c = '\0';
 	while (text[i])
+	{
+		if (text[i] == -1)
+		{
+			write(1, &text[j], k);
+			write(1,&c, 1);
+			j = i + 1;
+			k = -1;
+		}
 		i++;
+		k++;
+	}
 	if (i > 0)
-		write(1, text, i);
+		write(1, &text[j], k);
 	return (i);
 }

fd0cbacbcd69f768dc211f9f842ec977c7059ad7
SalemRibeiro <salem.ribeiro@sapo.pt>
2020-08-06 22:10:17 -0300

    inclusão de novos testes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sfreitas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/06 09:16:50 by sfreitas          #+#    #+#             */
-/*   Updated: 2020/08/06 09:16:56 by sfreitas         ###   ########.fr       */
+/*   Updated: 2020/08/06 22:07:35 by sfreitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@ int main ()
 //	test_int_signal_with_space();
 //	test_int_signal_with_hash();
 	ft_printf("Caractere:\n");
-	ft_printf("1 - %c, 2 - %c 12345%c6789\n", 'A', 'B','\0');
+	ft_printf("ft: %d\n",ft_printf("1 - %c, 2 - %c 12345%c6789\n", 'A', 'B','\0'));
+	printf("ft: %d\n",printf("1 - %c, 2 - %c 12345%c6789\n", 'A', 'B','\0'));
 	ft_printf("String\n");
-	ft_printf("texto = %s\n", "salem.ribeiro@gepro.eng.br");
+	ft_printf("ft: %d\n",ft_printf("texto = %s\n", "salem.ribeiro@gepro.eng.br"));
+	printf("pt: %d\n",printf("texto = %s\n", "salem.ribeiro@gepro.eng.br"));
 	return (0);
 }

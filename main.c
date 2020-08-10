0be13c6d68a3e61a58a288cecbe6a88f50717ac3
SalemRibeiro <salem.ribeiro@sapo.pt>
2020-08-10 09:11:00 -0300

    inclusão de novos testes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sfreitas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/06 09:16:50 by sfreitas          #+#    #+#             */
-/*   Updated: 2020/08/07 00:05:24 by sfreitas         ###   ########.fr       */
+/*   Updated: 2020/08/09 11:58:53 by sfreitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,39 @@
 #include <stdio.h>
 int main ()
 {
-	int		a = -4;
-	int		b = 0;
+//	int		a = -4;
+//	int		b = 0;
 	char	c = 'a';
 	int		d = 2147483647;
 	int		e = -2147483648;
-	int		f = 42;
-	int		g = 25;
-	int		h = 4200;
+//	int		f = 42;
+//	int		g = 25;
+//	int		h = 4200;
 	int		i = 8;
 	int		j = -12;
 	int		k = 123456789;
 	int		l = 0;
-/*	int		m = -12345678;
-	char	*n = "abcdefghijklmnop";
+	int		m = -12345678;
+/*	char	*n = "abcdefghijklmnop";
 	char	*o = "-a";
 	char	*p = "-12";
 	char	*q = "0";
 	char	*r = "%%";
 	char	*s = "-2147483648";
 	char	*t = "0x12345678";
-	char	*u = "-0";
-*/	
+	char	*u = "-0";*/
 
-	ft_printf(" --- Return : %d\n", ft_printf("%i, %d, %d, %d, %c, %c, %c, %d, %u, %x, %X\n", i, j, k, l, c, c, c, c, j, j, j));
+
+//ft_printf("O número é: %d\n", d);
+
+
+/*	ft_printf(" --- Return : %d\n", ft_printf("%i, %d, %d, %d, %c, %c, %c, %d, %u, %x, %X\n", i, j, k, l, c, c, c, c, j, j, j));
 	printf(" --- Return : %d\n\n", printf("%i, %d, %d, %d, %c, %c, %c, %d, %u, %x, %X\n", i, j, k, l, c, c, c, c, j, j, j)); //T2
-/*	
+*/	
 	ft_printf(" --- Return : %d\n", ft_printf("%0i, %0d, %0d, %0d, %0d, %0d, %0d, %0d\n", i, j, k, l, m, c, e, d)); //T3
 	printf(" --- Return : %d\n\n", printf("%0i, %0d, %0d, %0d, %0d, %0d, %0d, %0d\n", i, j, k, l, m, c, e, d)); //T3
 	
-	ft_printf(" --- Return : %d\n", ft_printf("%1i, %1d, %1d, %1d, %1d, %1d, %1d, %1d\n", i, j, k, l, m, c, e, d)); //T4
+/*	ft_printf(" --- Return : %d\n", ft_printf("%1i, %1d, %1d, %1d, %1d, %1d, %1d, %1d\n", i, j, k, l, m, c, e, d)); //T4
 	printf(" --- Return : %d\n\n", printf("%1i, %1d, %1d, %1d, %1d, %1d, %1d, %1d\n", i, j, k, l, m, c, e, d)); //T4
 	
 	ft_printf(" --- Return : %d\n", ft_printf("%2i, %2d, %2d, %2d, %2d, %2d, %2d, %2d\n", i, j, k, l, m, c, e, d)); //T5

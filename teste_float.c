5f7e12613aa3379fc8c27610bf06667f9fa2c184
salem.ribeiro <salem.ribeiro@sapo.pt>
2020-09-15 07:43:31 -0300

    revisão da nova lógica para lidar com ponto flutuante

--- teste_float.c
+++ teste_float.c
@@ -17,68 +17,39 @@
 #include "libft.h"
 
 
-int	solve_round(long double *value, int precision)
-{
-	long long int	ibase;
-	long double		fbase;
-	int				i;
 
+char	*convert_char(long long double value, int precision)
+{
+	long long int		v_int;
+	long long double	v_doub;
+	char				*c_number;
+	int					i;
+	char				flag;
+
+	v_int = 0;
+	v_doub = 0;
 	i = 0;
-	fbase = *value;
-	ibase = fbase;
-	fbase = (fbase - (long long double)ibase) * 10;
-	while (i < precision && fbase >= 0)
+	v_int = value;
+	flag = 0;
+	v_doub = v_doub - (long long double)v_int;
+	c_number = ft_calloc(precision + 1, size_t(char));
+	while (i < precision)
 	{
 		
+		c_number[i] = 
 		i++;
 	}
-	
 
-	ibase = value;
-	f
-	return (0);
+
+
+
+
 }
-
-int	type_float(long double fbase, int precision)
-{
-	char			*text;
-	long long int	ibase;
-	int				i;
-
-	ibase = fbase;
-	text = ft_itoa(ibase);
-	fbase = (fbase - (long double)ibase);
-	if (solve_round(&fbase, precision);
-		ibase++;
-	text = ft_strjoin(text, ".");
-}
-
-
-
 
 int main()
 {
 	char *texto;
 	long double f = 16.7738996666661033;
-	long long int i;
-	int count;
-	int precision;
 
-	printf("O valor real é %.0Lf\n", f);
-	precision = 8;
-	count = 0;
-	i = f;
-	texto = ft_itoa(i);
-	f = (f - (double)i) * 10;
-	texto = ft_strjoin(texto, ".");
-	i = f;
-	while (f >= 0 && count < (precision - 2))
-	{
-		texto = ft_strjoin(texto, ft_itoa(i));
-		f = (f - (double)i) * 10;
-		i = f;
-		count ++;
-	}
-	printf("valor = %s\n", texto);
 	return (0);
 }

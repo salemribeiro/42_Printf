4ee32e591911af6da0f5fd7196734bd36a54be66
salem.ribeiro <salem.ribeiro@sapo.pt>
2020-09-16 23:23:50 -0300

    inicio da interação de conversão para um ponteiro de char

--- teste_float.c
+++ teste_float.c
@@ -18,38 +18,42 @@
 
 
 
-char	*convert_char(long long double value, int precision)
+char	*convert_char(long double value, int precision)
 {
 	long long int		v_int;
-	long long double	v_doub;
+	long double			v_doub;
 	char				*c_number;
 	int					i;
 	char				flag;
 
 	v_int = 0;
 	v_doub = 0;
-	i = 0;
+	i = 1;
 	v_int = value;
 	flag = 0;
-	v_doub = v_doub - (long long double)v_int;
-	c_number = ft_calloc(precision + 1, size_t(char));
+	v_doub = v_doub - (unsigned long double)v_int;
+	c_number = ft_calloc(precision + 1, sizeof(char));
 	while (i < precision)
 	{
-		
-		c_number[i] = 
+		v_doub = v_doub * 10;
+		c_number[i] = v_doub;
+		v_doub = v_doub - c_number[i];
+		i++;
+	}
+	i = 0;
+	while (i < precision)
+	{
+		printf("%d -> %hhd\n", i, c_number[i]);
 		i++;
 	}
 
-
-
-
-
+	return (NULL);
 }
 
 int main()
 {
 	char *texto;
 	long double f = 16.7738996666661033;
-
+	convert_char(f, 3);
 	return (0);
 }

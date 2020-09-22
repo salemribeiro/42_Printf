e89c6a16b3efd828ecd544e588ed5d38d421852d
salem.ribeiro <salem.ribeiro@sapo.pt>
2020-09-22 02:15:11 -0300

    Inclusão de novos testes

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ int main ()
 //	char	*s = "-2147483648";
 //	char	*t = "0x12345678";
 //	char	*u = "-0";
-
+	float	ponto = -13.999999;
 
 
 ft_printf(" --- Return : %d\n\n", ft_printf("%i, %d, %d, %d, %c, %c, %c, %d, %u, %x, %X\n", i, j, k, l, c, c, c, c, j, j, j)); //T2
@@ -52,9 +52,16 @@ ft_printf(" --- Return : %d\n", ft_printf("%1i, %1d, %1d, %1d, %1d, %1d, %1d, %1
 	
 	ft_printf(" --- Return : %d\n", ft_printf("%2i, %2d, %2d, %2d, %2d, %2d, %2d, %2d\n", i, j, k, l, m, c, e, d)); //T5
 	printf(" --- Return : %d\n\n", printf("%2i, %2d, %2d, %2d, %2d, %2d, %2d, %2d\n", i, j, k, l, m, c, e, d)); //T5
-	
+
 	ft_printf(" --- Return : %d\n", ft_printf("%3i, %3d, %3d, %3d, %3d, %3d, %3d, %3d\n", i, j, k, l, m, c, e, d)); //T6
 	printf(" --- Return : %d\n\n", printf("%3i, %3d, %3d, %3d, %3d, %3d, %3d, %3d\n", i, j, k, l, m, c, e, d)); //T6
+
+ft_printf(" --- Return : %d\n", ft_printf("%3i, %3d, %3d, %3d, %3d, %3d, %3d, %.5f\n", i, j, k, l, m, c, e, ponto)); //T6
+	printf(" --- Return : %d\n\n", printf("%3i, %3d, %3d, %3d, %3d, %3d, %3d, %.5f\n", i, j, k, l, m, c, e, ponto)); //T6
+
+//ft_printf("valor flutuante = %.5Lf\n", ponto);
+
+
 	return (0);
 
 }

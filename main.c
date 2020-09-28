aa21029faebe03145aac2a4d86af44cd5361ccfe
salem.ribeiro <salem.ribeiro@sapo.pt>
2020-09-28 01:58:03 -0300

    inclusão de novos testes

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 int main ()
 {
 
-	float	ponto = 3.9950456789;
+	double	ponto = 563340.0000000000456777;
 
 // teste com especificador f
 	ft_printf("\nvalor flutuante = %f\n", ponto);
@@ -32,8 +32,11 @@ int main ()
 	printf("valor flutuante = %.4f\n", ponto);
 	ft_printf("\nvalor flutuante = %.5f\n", ponto);
 	printf("valor flutuante = %.5f\n", ponto);
+	ft_printf("\nvalor flutuante = %.10f\n", ponto);
+	printf("valor flutuante = %.10f\n", ponto);
 
-// teste com especificador g
+/*
+//	 teste com especificador g
 	ft_printf("\nvalor flutuante = %g\n", ponto);
 	printf("valor flutuante = %g\n", ponto);
 	ft_printf("\nvalor flutuante = %.0g\n", ponto);
@@ -48,22 +51,23 @@ int main ()
 	printf("valor flutuante = %.4g\n", ponto);
 	ft_printf("\nvalor flutuante = %.5g\n", ponto);
 	printf("valor flutuante = %.5g\n", ponto);
+*/
 
 // teste com especificador a
-	ft_printf("\nvalor flutuante = %f\n", ponto);
-	printf("valor flutuante = %a\n", ponto);
-	ft_printf("\nvalor flutuante = %.0f\n", ponto);
-	printf("valor flutuante = %.0a\n", ponto);
-	ft_printf("\nvalor flutuante = %.1f\n", ponto);
-	printf("valor flutuante = %.1a\n", ponto);
-	ft_printf("\nvalor flutuante = %.2f\n", ponto);
-	printf("valor flutuante = %.2a\n", ponto);
-	ft_printf("\nvalor flutuante = %.3f\n", ponto);
-	printf("valor flutuante = %.3a\n", ponto);
-	ft_printf("\nvalor flutuante = %.4f\n", ponto);
-	printf("valor flutuante = %.4a\n", ponto);
-	ft_printf("\nvalor flutuante = %.5f\n", ponto);
-	printf("valor flutuante = %.5a\n", ponto);
+	ft_printf("\nvalor flutuante = %e\n", ponto);
+	printf("valor flutuante = %e\n", ponto);
+	ft_printf("\nvalor flutuante = %.0e\n", ponto);
+	printf("valor flutuante = %.0e\n", ponto);
+	ft_printf("\nvalor flutuante = %.1e\n", ponto);
+	printf("valor flutuante = %.1e\n", ponto);
+	ft_printf("\nvalor flutuante = %.2e\n", ponto);
+	printf("valor flutuante = %.2e\n", ponto);
+	ft_printf("\nvalor flutuante = %.3e\n", ponto);
+	printf("valor flutuante = %.3e\n", ponto);
+	ft_printf("\nvalor flutuante = %.4e\n", ponto);
+	printf("valor flutuante = %.4e\n", ponto);
+	ft_printf("\nvalor flutuante = %.5e\n", ponto);
+	printf("valor flutuante = %.5e\n", ponto);
 
 	return (0);
 
